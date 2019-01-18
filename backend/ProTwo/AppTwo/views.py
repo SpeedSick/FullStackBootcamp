3d90a8fb0052c209fb177aa97dc8705bd5ed24d5
alan <alan@relog.kz>
2019-01-18 23:45:55 +0600

    django

---
+++ backend/ProTwo/AppTwo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<em> My Second App </em>')
+
+def help(request):
+    helpdict = {'page_from': 'help page'}
+    return render(request, 'AppTwo/help.html', context=helpdict)
