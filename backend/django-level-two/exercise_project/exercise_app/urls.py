7e7ca8eda0e0da706952e6a2585b9502e2e6bf0f
alan <alan@relog.kz>
2019-01-19 20:10:40 +0600

    ModelForm added

--- backend/django-level-two/exercise_project/exercise_app/urls.py
+++ backend/django-level-two/exercise_project/exercise_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from exercise_app.views import userView
+from exercise_app.views import userView, user_form_view
 
 urlpatterns = [
     url(r'^$', userView),
+    url(r'^user_form_page/', user_form_view),
 ]
--- backend/django-level-two/exercise_project/exercise_app/views.py
+++ backend/django-level-two/exercise_project/exercise_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from exercise_app.models import User
+from exercise_app.forms import UserForm
 
 # Create your views here.
 
@@ -14,3 +15,18 @@ def userView(request):
         'users': users,
     }
     return render(request, 'exercise_app/index.html', context=content)
+
+
+def user_form_view(request):
+    if request.method == 'POST':
+
+        form = UserForm(request.POST)
+        if form.is_valid():
+
+            new_user = User.objects.get_or_create(**form.cleaned_data)[0]
+
+            new_user.save()
+
+        return hello(request)
+
+    return render(request, 'exercise_app/user_form_page.html', {'form': UserForm})
